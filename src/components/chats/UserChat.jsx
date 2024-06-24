991224effb0c725846ebf5955a85308890d831e5
Sonai Barua <sonaiClayMind@gmail.com>
2024-06-24 17:57:17 +0530

    commit all

---
+++ src/components/chats/UserChat.jsx
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
+import { Stack } from "react-bootstrap";
+import avatar from "../../assets/avatar.svg";
+import { ChatContext } from "../../context/ChatContext";
+
+
+const UserChat = ({ chat, user }) => {
+  const { recipientUser } = useFetchRecipientUser(chat, user);
+  const {onlineUsers}=useContext(ChatContext);
+
+  console.log("RECIPIENT USER", recipientUser?.name);
+  const isOnline = onlineUsers?.some(
+    (onlineUser) => onlineUser?.userId === recipientUser?._id
+  );
+
+  return (
+    <Stack
+      direction="horizontal"
+      gap={3}
+      className="user-card align-items-center p-2 justify-content-between"
+      role="button"
+    >
+      <div className="d-flex">
+        <div className="me-2">
+            <img src={avatar} alt="avatar" height="35" />
+        </div>
+        <div className="text-content">
+          <div className="name">{recipientUser?.name}</div>
+          <div className="text">Text Message</div>
+        </div>
+      </div>
+      <div className="d-flex flex-column align-items-end">
+        <div className="date">12/06/2024</div>
+        <div className="this-user-notifications">2</div>
+        <span className={isOnline ? "user-online" : ""}></span>
+      </div>
+    </Stack>
+  );
+};
+
+export default UserChat;
