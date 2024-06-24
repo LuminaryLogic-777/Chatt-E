bb781c0dd4daeac5d79d8279be88afb3abcc6e9b
Sonai Barua <sonaiClayMind@gmail.com>
2024-06-24 18:06:26 +0530

    added domain

--- src/context/ChatContext.jsx
+++ src/context/ChatContext.jsx
@@ -23,7 +23,7 @@ export const ChatContextProvider = ({ children, user }) => {
   console.log("Online Users", onlineUsers);
 
   useEffect(()=>{
-    const newSocket=io("http://192.168.0.159:3000");
+    const newSocket=io("http://192.168.0.159:3000" || "https://chatt-e.vercel.app:3000");
     setSocket(newSocket);
 
     return ()=>newSocket.disconnect();
